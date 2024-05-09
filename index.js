2d8d41cd350c474f89c4421e53e257f705bef548
Luis Felipe <luisfelipe.di@gmail.com>
2024-05-08 20:01:56 -0400

    :tada: deploy Desafio Evaluado Modulo 6 Seccion2 Citas Medicas

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes/routes.js';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//routes 
+app.use("/", routes)
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor activo en http://localhost:${PORT}`);
+})
